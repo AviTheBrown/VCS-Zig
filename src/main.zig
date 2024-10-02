dd321c1b511436ad50f7efaa5d065693b4ebf0e7
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2024-10-01 20:37:59 -0400

    Auto-commit: Changes as of 2024-10-01 20:37:59

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,12 @@
 const std = @import("std");
 
 pub fn main() void {
-    std.debug.print("welcome\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator().init;
+    const allocator = gpa.allocator();
+    defer {
+        const deinit_status = gpa.deinit();
+        if (deinit_status == .leak) @panic("Memory Leak Detected");
+    }
+
+    var args = try std.process.argsWithAllocator(allocator);
 }
